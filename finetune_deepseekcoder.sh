6898004c276bcbad73f1a09b81f730e653d75ad7
ben <ben.hall17@outlook.com>
2024-07-18 10:51:05 +0100

    chore: Update batch size and gradient accumulation steps for finetuning

--- finetune_deepseekcoder.sh
+++ finetune_deepseekcoder.sh
@@ -8,10 +8,11 @@ MODEL="deepseek-ai/deepseek-coder-1.3b-instruct"
 # Change directory to finetune
 cd finetune
 
-# batchsize was 16
+# per_device_train_batch_size was 16
 # gradient_accumulation_steps was 4
 # at the bottom   --bf16 True
-# REMEMBER TO CHANGE THE DEEPSPEED CONFIG!
+# logging_steps was 100
+# save_total_limit was 100
 
 # Run the finetuning script with DeepSpeed
 deepspeed finetune_deepseekcoder.py \
@@ -20,16 +21,16 @@ deepspeed finetune_deepseekcoder.py \
     --output_dir $OUTPUT_PATH \
     --num_train_epochs 3 \
     --model_max_length 1024 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 100 \
     --save_total_limit 100 \
     --learning_rate 2e-5 \
     --warmup_steps 10 \
-    --logging_steps 1 \
+    --logging_steps 10 \
     --lr_scheduler_type "cosine" \
     --gradient_checkpointing True \
     --report_to "tensorboard" \
