0619c3396cc4fd8f053aeb88282af8a81b96f4a6
ben <ben.hall17@outlook.com>
2024-07-21 23:12:10 +0100

    chore: Update DeepSeek-Coder model version for finetuning

--- memory.py
+++ memory.py
@@ -1,18 +1,12 @@
 from transformers import AutoModel
 from deepspeed.runtime.zero.stage3 import estimate_zero3_model_states_mem_needs_all_live
 
-model = AutoModel.from_pretrained("deepseek-ai/deepseek-coder-1.3b-instruct")
-estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=1, num_nodes=1)
+model = AutoModel.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct")
+estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=2, num_nodes=1)
 
 
+#1.3b parameters | Setup with 1 node, 1 GPU per node.
 """
-/home/ben/Desktop/DeepSeek-Coder/.venv/lib/python3.12/site-packages/transformers/utils/generic.py:441: UserWarning: torch.utils._pytree._register_pytree_node is deprecated. Please use torch.utils._pytree.register_pytree_node instead.
-  _torch_pytree._register_pytree_node(
-[2024-07-18 11:07:39,584] [INFO] [real_accelerator.py:158:get_accelerator] Setting ds_accelerator to cuda (auto detect)
-/home/ben/Desktop/DeepSeek-Coder/.venv/lib/python3.12/site-packages/transformers/utils/generic.py:309: UserWarning: torch.utils._pytree._register_pytree_node is deprecated. Please use torch.utils._pytree.register_pytree_node instead.
-  _torch_pytree._register_pytree_node(
-/home/ben/Desktop/DeepSeek-Coder/.venv/lib/python3.12/site-packages/transformers/utils/generic.py:309: UserWarning: torch.utils._pytree._register_pytree_node is deprecated. Please use torch.utils._pytree.register_pytree_node instead.
-  _torch_pytree._register_pytree_node(
 Estimated memory needed for params, optim states and gradients for a:
 HW: Setup with 1 node, 1 GPU per node.
 SW: Model with 1280M total params, 66M largest layer params.
@@ -23,4 +17,46 @@ SW: Model with 1280M total params, 66M largest layer params.
    28.62GB |   2.63GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
     0.37GB |  21.71GB | offload_param=none, offload_optimizer=none, zero_init=1
     7.15GB |  21.71GB | offload_param=none, offload_optimizer=none, zero_init=0
+"""
+
+#6.7b parameters | Setup with 1 node, 1 GPU per node.
+"""
+Estimated memory needed for params, optim states and gradients for a:
+HW: Setup with 1 node, 1 GPU per node.
+SW: Model with 6608M total params, 132M largest layer params.
+  per CPU  |  per GPU |   Options
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+  147.71GB |  12.80GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  147.71GB |  12.80GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+    0.74GB | 111.27GB | offload_param=none, offload_optimizer=none, zero_init=1
+   36.93GB | 111.27GB | offload_param=none, offload_optimizer=none, zero_init=0
+"""
+
+#6.7b parameters | Setup with 1 node, 4 GPU per node.
+"""
+Estimated memory needed for params, optim states and gradients for a:
+HW: Setup with 1 node, 4 GPUs per node.
+SW: Model with 6608M total params, 132M largest layer params.
+  per CPU  |  per GPU |   Options
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+  147.71GB |   3.57GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  147.71GB |   3.57GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+    2.95GB |  28.19GB | offload_param=none, offload_optimizer=none, zero_init=1
+  147.71GB |  28.19GB | offload_param=none, offload_optimizer=none, zero_init=0
+"""
+
+#6.7b parameters | Setup with 1 node, 2 GPU per node.
+"""
+Estimated memory needed for params, optim states and gradients for a:
+HW: Setup with 1 node, 2 GPUs per node.
+SW: Model with 6608M total params, 132M largest layer params.
+  per CPU  |  per GPU |   Options
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  166.17GB |   0.49GB | offload_param=OffloadDeviceEnum.cpu, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+  147.71GB |   6.65GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=1
+  147.71GB |   6.65GB | offload_param=none, offload_optimizer=OffloadDeviceEnum.cpu, zero_init=0
+    1.48GB |  55.88GB | offload_param=none, offload_optimizer=none, zero_init=1
+   73.85GB |  55.88GB | offload_param=none, offload_optimizer=none, zero_init=0
 """
